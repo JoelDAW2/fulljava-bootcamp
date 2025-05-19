9e64e725bbd9e1157b9d1ba3c42cf6a80ab3b2bf
JoelVRV <145133218+JoelDAW2@users.noreply.github.com>
2025-05-19 12:26:38 +0200

    ---

--- exercises_2/src/main/java/com/example/exercise1.java
+++ exercises_2/src/main/java/com/example/exercise1.java
@@ -2,13 +2,46 @@ package com.example;
 
 public class exercise1 {
     public static int factorialNumber(int num)
-    {   
-        int factorialNumber = 1;
-        for (int i = 1; i <= num; i++) {
-            factorialNumber = factorialNumber * 1;  
+    {
+        int factorial = 1;
+        for (int i = num; i > 0; i--) {
+            factorial = factorial * i;
         }
-        return factorialNumber;
+        return factorial;
+    }
+
+    private static boolean assertEquals(int expected, int actual)
+    {
+        if(expected == actual)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private static void testFactorialNumber(int factorialNum, int result)
+    {
+        if(!assertEquals(factorialNumber(factorialNum), result))
+        {
+            System.out.println("Test failed!");
+        }
+        else 
+        {
+            System.out.println("Test completed!");
+        }
     }
     public static void main(String[] args) {
+        testFactorialNumber(1, 1);
+        testFactorialNumber(2, 2);
+        testFactorialNumber(3, 6);
+        testFactorialNumber(4, 24);
+        testFactorialNumber(5, 120);
+        testFactorialNumber(6, 720);
+        testFactorialNumber(7, 5040);
+        testFactorialNumber(8, 40320);
+
     }
 }
--- exercises_2/src/main/java/com/example/exercise5.java
+++ exercises_2/src/main/java/com/example/exercise5.java
@@ -1,4 +1,5 @@
 package com.example;
+import java.util.ArrayList;
 
 public class exercise5 {
     // EJERCICIO 5
@@ -9,6 +10,25 @@ public class exercise5 {
             System.out.println(array[i]);
         }
     } */
+
+   public static void reverseArray(int[] array)
+    {   
+        ArrayList<Integer> reversedArray = new ArrayList<Integer>();
+
+        for(int num : array) {
+            System.out.println(num);
+        }
+
+        /* for (int i = array.length; i > 0; i--) {
+            // reversedArray.add(array[i]);
+            System.out.println(array[i]);
+        } */
+        for(int num : reversedArray) {
+            System.out.println(num);
+        }
+    }
     public static void main(String[] args) {
+        int[] listOfNums = new int[]{1, 2, 3};
+        reverseArray(listOfNums);
     }
 }
