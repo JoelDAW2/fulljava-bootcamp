b26535e8d41cbed3d211fdba2406a215ab4a1a87
JoelVRV <145133218+JoelDAW2@users.noreply.github.com>
2025-05-19 14:20:43 +0200

    MAS EJERCICIOS

--- exercises_2/src/main/java/com/example/exercise13.java
+++ exercises_2/src/main/java/com/example/exercise13.java
@@ -1,5 +1,30 @@
 package com.example;
 
 public class exercise13 {
-    
+    public static void primeNumbersInRange(int a, int b)
+    {
+        int low = a, high = b;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+            {
+                System.out.print(low + " ");
+            }
+
+            ++low;
+        }
+    }
+
+    public static void main(String[] args) {
+        primeNumbersInRange(10, 20);
+    }
 }
