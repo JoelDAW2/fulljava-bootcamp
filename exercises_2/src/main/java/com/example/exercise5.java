043fe55e34cab6da427055d87537e521d99b0df3
JoelVRV <145133218+JoelDAW2@users.noreply.github.com>
2025-05-19 13:09:33 +0200

    ---

--- exercises_2/src/main/java/com/example/exercise5.java
+++ exercises_2/src/main/java/com/example/exercise5.java
@@ -1,34 +1,44 @@
 package com.example;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class exercise5 {
-    // EJERCICIO 5
-    /* public static void reverseArray(int[] array)
-    {
-        int[] initialArray = array;
-        for (int i = array.length; i > 0; i--) {
-            System.out.println(array[i]);
-        }
-    } */
-
    public static void reverseArray(int[] array)
     {   
         ArrayList<Integer> reversedArray = new ArrayList<Integer>();
+        // String orderedArray = "[";
 
-        for(int num : array) {
-            System.out.println(num);
+        for (int i = array.length - 1; i >= 0; i--) {
+            // orderedArray += Integer.toString(array[i]);
+            reversedArray.add(array[i]);
         }
+        // orderedArray += "]";
+        // System.out.println(orderedArray);
+        String result = reversedArray.toString();
 
-        /* for (int i = array.length; i > 0; i--) {
-            // reversedArray.add(array[i]);
-            System.out.println(array[i]);
-        } */
-        for(int num : reversedArray) {
-            System.out.println(num);
-        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(result);
     }
+/* 
+    private static boolean assertEquals(Boolean expected, Boolean actual)
+    {
+        return expected.equals(actual);
+    }
+
+    private static void testReverseArrau(String word)
+    {
+        if(!assertEquals(reverseArray(array), true))
+        {
+            System.out.println("Test failed!");
+        }
+        else 
+        {
+            System.out.println("Test completed!");
+        }
+    } */
+
     public static void main(String[] args) {
-        int[] listOfNums = new int[]{1, 2, 3};
+        int[] listOfNums = { 6, 7, 8, 0, 2, 6, 8 };
         reverseArray(listOfNums);
     }
 }
