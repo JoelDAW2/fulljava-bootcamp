fd432c6f1fc504e2fccec82fdcd2d5ecf22c3997
JoelVRV <joelcospfp@ibf.cat>
2025-05-21 16:12:15 +0200

    POO: LISTA DE TAREAS

---
+++ POO/lista_de_tareas/src/main/java/com/example/Main.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import java.lang.Thread.State;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        List<Task> tasks = new ArrayList<>();
+
+        System.out.println("\n<<< LISTA DE TAREAS >>>\n");
+        Task task1 = new Task("Deberes de Java", "Acabar los deberes de Java de POO", Task.State.completed);
+        Task task2 = new Task("Comprar el pan", "Comprar pan en el Condis", Task.State.completed);
+        Task task3 = new Task("Sacar al perro", "Sacar al perro 15 minutos", Task.State.completed);
+        Task task4 = new Task("Comprar agua", "Comprar agua en el Spar", Task.State.uncompleted);
+        
+        tasks.add(task1);
+        tasks.add(task2);
+        tasks.add(task3);
+        tasks.add(task4);
+
+        /* for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + tasks.get(i).getTitle() + " --- " + tasks.get(i).getState());
+        } */
+
+        Task.listTasks(tasks);
+
+        System.out.print("-> Actualizar tarea? [y/n]: ");
+        String r = s.nextLine();
+
+        if (r.equalsIgnoreCase("y")) {
+            System.out.print("\nSelecciona una tarea: ");
+            int num = s.nextInt() - 1;
+            if(num > 0 && num < tasks.size())
+            {
+                System.out.print("\n---> Introduce el estado de la tarea [ 0 (completed) / 1 (uncompleted) ]: ");
+                int newState = s.nextInt();
+                
+                if (newState == 0) {
+                    tasks.get(num).setState(Task.State.completed);
+                }
+                else
+                {
+                    tasks.get(num).setState(Task.State.uncompleted);
+                }
+
+                Task.listTasks(tasks);
+            }
+            else
+            {
+                System.out.print("-> Tarea no existente. Vuelve a probar.");
+            }
+        }   
+    }
+}
---
+++ POO/lista_de_tareas/src/main/java/com/example/Task.java
@@ -0,0 +1,69 @@
+package com.example;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Task {
+
+    enum State {
+        completed,
+        uncompleted
+    }
+
+    private String title;
+    private String description;
+    private State state;
+
+    public Task(String title, String description, State state)
+    {
+        this.title = title;
+        this.description = description;
+        this.state = state;
+    }
+
+    // Getters
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public State getState()
+    {
+        return this.state;
+    }
+
+    // Setters
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public void setState(State state)
+    {
+        this.state = state;
+    }
+
+    public static void listTasks(List<Task> tasks)
+    {
+        System.out.println();
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + tasks.get(i).getTitle() + " --- " + tasks.get(i).getState());
+        }
+        System.out.println();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "\nTitle: " + title + ", " + "\nDescription: " + description + ", " + "\nstate: " + state; 
+    }
+}
