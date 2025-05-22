e9c83b6ca21cb20300b6400cbe849988214cfbf5
JoelVRV <145133218+JoelDAW2@users.noreply.github.com>
2025-05-22 10:39:36 +0200

    UPGRADES
    - La lista de tareas ya permite crear nuevas tareas.
    
    - Inicio ejercicio gestor de encuestas

---
+++ POO/gestor_de_cuentas/src/main/java/com/example/Survey.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Survey {
+    private String title;
+    private String[][] questions = { 
+        {
+            "¿Que animal es el rey de la sabana?",
+            "¿Que animal tiene el cuello muy largo?",
+            "¿Que animal tiene trompa?",
+        }, 
+        {
+            "Leon",
+            "Jirafa",
+            "Eelefante"
+        }
+    };
+}
--- POO/lista_de_tareas/src/main/java/com/example/Main.java
+++ POO/lista_de_tareas/src/main/java/com/example/Main.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import java.lang.Thread.State;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +9,8 @@ public class Main {
         Scanner s = new Scanner(System.in);
 
         List<Task> tasks = new ArrayList<>();
+
+        boolean phone = true;
 
         System.out.println("\n<<< LISTA DE TAREAS >>>\n");
         Task task1 = new Task("Deberes de Java", "Acabar los deberes de Java de POO", Task.State.completed);
@@ -24,10 +25,10 @@ public class Main {
 
         Task.listTasks(tasks);
 
-        System.out.print("-> Actualizar tarea? [y/n]: ");
-        String r = s.nextLine();
+        /* System.out.print("-> Actualizar tarea | Crear tarea [1/2]: ");
+        int r = s.nextInt();
 
-        if (r.equalsIgnoreCase("y")) {
+        if (r == 1) {
             System.out.print("\n-> Selecciona una tarea: ");
             int num = s.nextInt();
             if(num >= 0 && num < tasks.size())
@@ -50,9 +51,99 @@ public class Main {
                 System.out.print("-> Tarea no existente. Vuelve a probar.");
             }
         } 
-        else 
+        else if(r == 2)
+        {
+            System.out.print("\n-> Nombre de la nueva tarea: ");
+            String newTask = s.nextLine();
+            System.out.print("\n-> Descripción de la nueva tarea: ");
+            String newDescription = s.nextLine();
+            System.out.print("\n-> Estado de la nueva tarea [ 0 (completed) / 1 (uncompleted) ]: ");
+            int actualNewState = s.nextInt();
+            if (actualNewState == 0) {
+                Task n = new Task(newTask, newDescription, Task.State.completed);
+                tasks.add(n);
+            }
+            else
+            {
+                Task n = new Task(newTask, newDescription, Task.State.completed);
+                tasks.add(n);
+            }
+        }
+        else
         {
             System.out.print("-> De acuerdo.");
-        }  
+        } */  
+
+        System.out.println("\n<<< LISTA DE TAREAS >>>\n");
+        
+        do { 
+            System.out.println("\n1. Listar tareas");
+            System.out.println("2. Actualizar tarea");
+            System.out.println("3. Crear tarea");
+            System.out.println("4. Salir");
+
+            System.out.print("\n-> Seleccione una opción: ");
+            
+            int option = s.nextInt();
+
+            if (option < 1 || option > 4) {
+                System.out.println("\n** ERROR: Opción fuera de rango **");
+            }
+            else
+            {
+                Scanner sca = new Scanner(System.in);
+                switch(option)
+                {
+                    case 1:
+                        Task.listTasks(tasks);
+                        break;
+                    case 2:
+                        System.out.print("\n-> Selecciona una tarea: ");
+                        int num = s.nextInt();
+                        if(num >= 0 && num < tasks.size())
+                        {
+                            System.out.print("\n-> Introduce el estado de la tarea [ 0 (completed) / 1 (uncompleted) ]: ");
+                            int newState = s.nextInt();
+                            
+                            if (newState == 0) {
+                                tasks.get(num).setState(Task.State.completed);
+                            }
+                            else
+                            {
+                                tasks.get(num).setState(Task.State.uncompleted);
+                            }
+
+                            Task.listTasks(tasks);
+                        }
+                        else
+                        {
+                            System.out.print("-> Tarea no existente. Vuelve a probar.");
+                        }
+                        break;
+                    case 3:
+                        Scanner newTaskSca = new Scanner(System.in);
+                        System.out.print("\n-> Nombre de la nueva tarea: ");
+                        String newTask = newTaskSca.nextLine();
+                        System.out.print("\n-> Descripción de la nueva tarea: ");
+                        String newDescription = newTaskSca.nextLine();
+                        System.out.print("\n-> Estado de la nueva tarea [ 0 (completed) / 1 (uncompleted) ]: ");
+                        int actualNewState = newTaskSca.nextInt();
+                        if (actualNewState == 0) {
+                            Task n = new Task(newTask, newDescription, Task.State.completed);
+                            tasks.add(n);
+                        }
+                        else
+                        {
+                            Task n = new Task(newTask, newDescription, Task.State.uncompleted);
+                            tasks.add(n);
+                        }
+                        break;
+                    case 4:
+                        phone = false;
+                        break;
+                }
+            }
+        }
+        while(phone != false);
     }
 }
