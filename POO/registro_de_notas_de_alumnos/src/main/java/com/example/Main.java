cc551d299c8813b41a23bcc933c59f4a73e6c4ad
JoelVRV <145133218+JoelDAW2@users.noreply.github.com>
2025-05-22 10:05:20 +0200

    POO: REGISTRO DE NOTAS DE ALUMNOS
    - Falta el punto extra de mostrar el mejor alumno

--- POO/registro_de_notas_de_alumnos/src/main/java/com/example/Main.java
+++ POO/registro_de_notas_de_alumnos/src/main/java/com/example/Main.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student s1 = new Student("Tony", List.of(9.5, 9.5, 9.5));
+        Student s2 = new Student("Steve", List.of(9.5, 8.5, 9.5));
+        Student s3 = new Student("Thor", List.of(3.5, 4.5, 2.5));
+        Student s4 = new Student("Natasha", List.of(7.5, 6.5, 9.5));
+
+        System.out.println("<<< PROMEDIO DE CADA ALUMNO >>>\n");
+        System.out.println(s1.getName() + ": " + s1.calculateAverage() + s1.studentApproves());
+        System.out.println(s2.getName() + ": " + s2.calculateAverage() + s2.studentApproves());
+        System.out.println(s3.getName() + ": " + s3.calculateAverage() + s3.studentApproves());
+        System.out.println(s4.getName() + ": " + s4.calculateAverage() + s4.studentApproves());
+        System.out.println();
+        
+        // System.out.println("Mejor alumno: " + Student.bestStudent(s1, s2, s3, s4));
     }
 }
--- POO/registro_de_notas_de_alumnos/src/main/java/com/example/Student.java
+++ POO/registro_de_notas_de_alumnos/src/main/java/com/example/Student.java
@@ -1,5 +1,71 @@
 package com.example;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
     private String name;
+    // private List myList = new ArrayList();
+    private List<Double> notes = new ArrayList<>();
+
+    public Student(String name, List<Double> notes)
+    {
+        this.name = name;
+        this.notes = notes;
+    }
+
+    // Getters
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public List<Double> getNotes()
+    {
+        return this.notes;
+    }
+
+    // Setters
+    public void setName(String title)
+    {
+        this.name = name;
+    }
+
+    public void setNotes(List<Double> notes)
+    {
+        this.notes = notes;
+    }
+
+    public void addNotes(Double note)
+    {
+        this.notes.add(note);
+    }
+
+    public Double calculateAverage()
+    {
+        Double sumNotes = 0.0;
+        for (Double note : this.notes) {
+            sumNotes += note;
+        }
+        return sumNotes / this.notes.size();
+    }
+
+    public String studentApproves()
+    {
+        if(this.calculateAverage() >= 5)
+        {
+            return " (Aprobado)";
+        }
+        return " (Suspendido)";
+    }
+
+    /* public static Student bestStudent(List<Student> students)
+    {
+        List<Student> orderedStudents = new ArrayList<>();
+        List<Double> studentsNotes = new ArrayList<>();
+        for (Student student : students) {
+            studentsNotes.add(student.calculateAverage());
+        }
+        Collections.sort(studentsNotes);
+        return studentsNotes.get(studentsNotes.size());
+    } */
 }
